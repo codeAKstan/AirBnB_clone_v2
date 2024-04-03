1c8a64cfc5c3204307e177416a7aae18297ce59a
kmmadu <mmadubugwukingsley@gmail.com>
2024-04-03 23:46:38 +0100

    adjustment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,49 +1,27 @@
 #!/usr/bin/env bash
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
 
-# Install Nginx if it's not already installed
-apt-get update
-apt-get -y install nginx
-
-# Create necessary directories if they don't exist
-mkdir -p /data/web_static/releases/test
-mkdir -p /data/web_static/shared
-
-# Create a fake HTML file for testing Nginx configuration
-echo "<html>
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" > /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create or update symbolic link
-rm -rf /data/web_static/current
-ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# Give ownership of the /data/ folder to the ubuntu user and group
-chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
-# Update Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-config_path="/etc/nginx/sites-available/default"
-echo "
-server {
-    listen 80;
-    listen [::]:80 default_server;
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
-    server_name _;
-
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}" > "$config_path"
-
-# Restart Nginx
-service nginx restart
-
-exit 0
-
+sudo service nginx restart
